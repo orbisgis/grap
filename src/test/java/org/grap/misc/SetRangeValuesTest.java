660b4c4d4fdb218603d601eb7b6643761750fba2
ebocher <ebocher@irstv>
2008-02-26 17:19:54 +0000

    For Thomas : need to be complete Don't forget a setrange on color_256 create a grayscale georaster. It's normal. It's imageJ model.

---
+++ src/test/java/org/grap/misc/SetRangeValuesTest.java
@@ -0,0 +1,25 @@
+package org.grap.misc;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import org.grap.io.GrapTest;
+import org.grap.model.GeoRaster;
+import org.grap.model.GeoRasterFactory;
+
+public class SetRangeValuesTest extends GrapTest {
+	private final static int RED = Color.RED.getRGB();
+
+	public void testTransparency() throws Exception {
+		final GeoRaster gr = GeoRasterFactory.createGeoRaster(externalData + "/geotif/440606.tif");
+		gr.open();
+
+		gr.setRangeValues(0, 10);
+		
+		gr.show();
+
+		
+	}
+}
