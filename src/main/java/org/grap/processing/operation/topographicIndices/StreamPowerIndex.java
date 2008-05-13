92cee25debf41c27d0cbf8c373d42f1b1ef225b4
ebocher <ebocher@irstv>
2008-05-13 10:05:15 +0000

    Topographic indexes

---
+++ src/main/java/org/grap/processing/operation/topographicIndices/StreamPowerIndex.java
@@ -0,0 +1,139 @@
+/*
+ * The GRAP library (GeoRAster Processing) is a middleware dedicated
+ * to the processing of various kinds of geographic raster data. It
+ * provides a complete and robust API to manipulate ASCII Grid or
+ * tiff, png, bmp, jpg (with the corresponding world file) geographic
+ * images. GRAP is produced  by the geomatic team of the IRSTV Institute
+ * <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
+ *    Erwan BOCHER, scientific researcher,
+ *    Thomas LEDUC, scientific researcher,
+ *    Fernando GONZALEZ CORTES, computer engineer.
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * This file is part of GRAP.
+ *
+ * GRAP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GRAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GRAP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult:
+ *    <http://orbisgis.cerma.archi.fr/>
+ *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-developers/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-users/>
+ *
+ * or contact directly:
+ *    erwan.bocher _at_ ec-nantes.fr
+ *    fergonco _at_ gmail.com
+ *    thomas.leduc _at_ cerma.archi.fr
+ */
+package org.grap.processing.operation.topographicIndices;
+
+import ij.process.ImageProcessor;
+
+import java.io.IOException;
+
+import org.grap.io.GeoreferencingException;
+import org.grap.model.GeoRaster;
+import org.grap.model.GeoRasterFactory;
+import org.grap.processing.Operation;
+import org.grap.processing.OperationException;
+import org.grap.utilities.PixelsUtil;
+
+
+
+
+public class StreamPowerIndex implements Operation {
+	
+
+	private static final double ALMOST_ZERO = 0.0011;
+	
+	private int ncols;
+	private int nrows;
+	private ImageProcessor m_Slope;
+	private ImageProcessor slope;
+	private GeoRaster accFlow;
+	private ImageProcessor m_StreamPowerIndex;
+	private ImageProcessor m_accFlow;
+	private float cellSize;
+	
+	public StreamPowerIndex(final GeoRaster accFlow){
+		this.accFlow=accFlow;
+	}
+
+	public GeoRaster execute(final GeoRaster geoRaster)
+			throws OperationException, GeoreferencingException {
+	
+		return processAlgorithm(geoRaster);
+	}
+	
+	
+	
+	public GeoRaster processAlgorithm(final GeoRaster geoRaster){
+		
+		
+		
+		try {
+			m_Slope = geoRaster.getGrapImagePlus().getProcessor();
+			m_accFlow = accFlow.getGrapImagePlus().getProcessor();
+			
+			
+			
+			nrows = geoRaster.getMetadata().getNRows() ;
+			ncols = geoRaster.getMetadata().getNCols();
+			cellSize = geoRaster.getMetadata().getPixelSize_X();
+			
+			m_StreamPowerIndex =   m_Slope.duplicate();
+			
+			m_StreamPowerIndex.multiply(0);
+			
+			
+			int x,y;
+
+			for(y = 0; y<ncols; y++){
+				for(x = 0; x < nrows; x++){
+					
+					float dSlope = m_Slope.getPixelValue(x, y);
+					float dAccFlow = m_accFlow.getPixelValue(x, y);
+
+					if (((Float.isNaN(dSlope))||(Float.isNaN(dAccFlow)))){
+						m_StreamPowerIndex.putPixelValue(x, y, Float.NaN);
+						
+					}
+					else {
+						dAccFlow /= cellSize;
+						dSlope = (float) Math.max(Math.tan(dSlope),  ALMOST_ZERO);
+						m_StreamPowerIndex.putPixelValue(x, y, dAccFlow * dSlope);
+					}
+				}
+			}
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (GeoreferencingException e) {
+			e.printStackTrace();
+		}
+	
+		
+		
+		
+		
+		return GeoRasterFactory.createGeoRaster(m_StreamPowerIndex, geoRaster.getMetadata());
+		
+	}
+	
+	
+	
+
+}
